5a126474c2c3ff1e2cf49ff6400f3abf6869f33e
joaoboger <joaoboger@gmail.com>
2023-05-20 20:41:33 -0300

    Organizing files and folders

--- pipeline/churn_pipeline.py
+++ pipeline/churn_pipeline.py
@@ -2,11 +2,11 @@ import pandas as pd
 import pickle
 
 # Load the fitted pipeline from the file
-with open('fitted_pipeline.pkl', 'rb') as file:
+with open('../output/fitted_pipeline.pkl', 'rb') as file:
     pipeline = pickle.load(file)
 
 # Load the data to be predicted from the CSV file
-data = pd.read_csv('./Abandono_teste.csv', delimiter=';')
+data = pd.read_csv('../data/Abandono_teste.csv', delimiter=';')
 
 # Extract the features from the data
 X = data.drop(['RowNumber', 'CustomerId', 'Surname'], axis=1)  # Exclude unused columns
@@ -18,4 +18,4 @@ predictions = pipeline.predict(X)
 output_data = pd.DataFrame({'rowNumber': range(1, X.shape[0] + 1), 'predictedValues': predictions})
 
 # Save the predictions to a CSV file
-output_data.to_csv('predictions.csv', index=False)
+output_data.to_csv('../output/predictions.csv', index=False)
